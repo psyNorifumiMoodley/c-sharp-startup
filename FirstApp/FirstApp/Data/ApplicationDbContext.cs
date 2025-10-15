d519d02bcd95bcd88df549c9dfdce4f01e2b31d6
sevvanamnorifumimoodley <norifumi.moodley@psybergate.co.za>
2025-10-15 09:41:16 +0200

    [COMPLETED] basic setup and customers

---
+++ FirstApp/FirstApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FirstApp.Models.Enitities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+}
---
+++ FirstApp/FirstApp/Models/CustomerRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstApp.Models;
+
+public class CustomerRequest
+{
+        
+    public required string FirstName { get; set; }
+    
+    public required string LastName { get; set; }
+}
